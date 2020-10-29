5ed08fceeb6b91c8b916520ace6ada4a10df172f
Alejandro Pérez Martín <aleperez92@gmail.com>
2020-10-29 11:57:36 +0000

    Update dependencies and add tests

--- test/rules.test.js
+++ test/rules.test.js
@@ -1,29 +1,11 @@
 const stylelint = require('stylelint');
 const config = require('../');
 
-const validCss = `
-p {
-  color: white;
-}
-`;
+const errorsExpected = 12;
 
-describe("flags no warnings with valid css", () => {
-  let result;
-
-  beforeEach(() => {
-    result = stylelint.lint({
-      code: validCss,
-      config,
-    });
-  });
-
-  it("did not error", () => {
-    return result.then((data) => expect(data.errored).toBeFalsy());
-  });
-
-  it("flags no warnings", () => {
-    return result.then((data) =>
-      expect(data.results[0].warnings).toHaveLength(0)
-    );
-  });
-});
+test('flags stylelint errors', () => (
+  stylelint.lint({
+    files: 'example/*.css',
+    config
+  }).then(data => expect(data.results[0].warnings).toHaveLength(errorsExpected))
+));
